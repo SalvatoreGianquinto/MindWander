33ff871d958285da27a9cb5ed11b3db8f98b018b
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-06-30 11:41:44 +0200

    inizio progetto

---
+++ src/main/java/it/epicode/MindWander/MindWanderApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.MindWander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindWanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindWanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/MindWander/MindWanderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.MindWander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindWanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
