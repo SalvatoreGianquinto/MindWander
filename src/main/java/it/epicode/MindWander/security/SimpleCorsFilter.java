2a86e07ebdaece54c962db8457187401b418fcd0
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-10-07 13:52:14 +0200

    Update SimpleCorsFilter.java

--- src/main/java/it/epicode/MindWander/security/SimpleCorsFilter.java
+++ src/main/java/it/epicode/MindWander/security/SimpleCorsFilter.java
@@ -7,11 +7,14 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SimpleCorsFilter implements Filter {
 
     @Override
